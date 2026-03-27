28497188e6975b0debee8b79305926e7abda4800
unknown <mustafakck.dev@gmail.com>
2026-03-27 12:09:57 +0300

    scaffold

---
+++ Northwind.Mvc/Models/CategorySalesFor1997.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Northwind.Mvc.Models;
+
+public partial class CategorySalesFor1997
+{
+    public string CategoryName { get; set; } = null!;
+
+    public decimal? CategorySales { get; set; }
+}
